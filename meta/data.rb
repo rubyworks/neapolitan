bae9443234d816cc5aea611f9cacb680d893ac1e
7rans <transfire@gmail.com>
2010-09-10 08:19:18 -0400

    admin: update metadata

---
+++ meta/data.rb
@@ -0,0 +1,27 @@
+Object.__send__(:remove_const, :VERSION) if Object.const_defined?(:VERSION)      # becuase Ruby 1.8~ gets in the way
+
+module Chocolates
+
+  DIRECTORY = File.dirname(__FILE__)
+
+  def self.gemfile
+    @gemfile ||= (
+      require 'yaml'
+      YAML.load(File.new(DIRECTORY + '/gemfile'))
+    )
+  end
+
+  def self.profile
+    @profile ||= (
+      require 'yaml'
+      YAML.load(File.new(DIRECTORY + '/profile'))
+    )
+  end
+
+  def self.const_missing(name)
+    key = name.to_s.downcase
+    gemfile[key] || profile[key] || super(name)
+  end
+
+end
+
