0e558782552ab8b84b19678a8b05bc4171f37b4f
7rans <transfire@gmail.com>
2010-11-08 21:46:59 -0500

    minor: deprecated Document class

--- lib/neapolitan.rb
+++ lib/neapolitan.rb
@@ -4,6 +4,11 @@ require 'neapolitan/template'
 module Neapolitan
 
   #
+  def self.new(source, options={})
+    Template.new(source, options)
+  end
+
+  #
   def self.file(fname, options={})
     Template.new(File.new(fname), options)
   end
