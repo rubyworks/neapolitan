b996b0f561e698a74422225ba18b28edf99fce8f
7rans <transfire@gmail.com>
2010-11-09 09:48:46 -0500

    test: add faq about using select and reject

--- doc/applique/env.rb
+++ doc/applique/env.rb
@@ -1,3 +1,11 @@
+directory = File.dirname(__FILE__)
+
+$LOAD_PATH <<  directory + '/../lib'
+
+require 'neapolitan'
+
+FileUtils.install(Dir[directory + '/../fixtures/*'], '.')
+
 When "Here is an example Neapolitan template, '(((.*?)))'" do |file, text|
   File.open(file, 'w'){ |f| f << text }
 end
